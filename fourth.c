0928ee47b1a861127dbddbce426163a417408aae
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-01-21 08:57:31 +0200

    6

--- fourth.c
+++ fourth.c
@@ -74,7 +74,7 @@ void pop(stack_t **s, unsigned int line_num)
 
 	if (!(args->head))
 	{
-		fprintf(stderr, "L%d: can't pop, stack empty\n", line_num);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_num);
 		all_free();
 		exit(EXIT_FAILURE);
 	}
